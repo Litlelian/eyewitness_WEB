e648d5eb5435a0049af09efe143b9da28d81eb8e
Littlelian <littlelian5269@gmail.com>
2025-09-10 18:06:03 +0800

    Can navigate to game page without kill room (not rebuild socket yet)

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./GamePage.css";
 
-export default function PlayerBoard({ playerID, roomData }) {
-  // 建立一個陣列 [0, 1, 2, ... N-1]
-  const players = Array.from({ length: length(roomData.players) }, (_, i) => i);
+export default function GamePage() {
+  const location = useLocation();
+  const { playerID, room } = location.state || {};
+
+  if (!room) {
+    return <div>房間資料不存在，請從房間進入遊戲。</div>;
+  }
 
   return (
     <div className="board-container">
       {/* 上方玩家區域 */}
       <div className="player-row">
-        {players.map((p) => (
-          <div key={p} className="rect player-rect">
-            玩家 {p + 1}
+        {room.players.map((p) => (
+          <div
+            key={p.id}
+            className={`rect player-rect ${p.id === playerID ? "self-player" : ""}`}
+          >
+            {p.name}
           </div>
         ))}
       </div>
 
-      {/* 下方兩個額外的長方形 */}
+      {/* 下方兩個額外長方形 */}
       <div className="extra-row">
         <div className="rect extra-rect">額外 1</div>
         <div className="rect extra-rect">額外 2</div>
